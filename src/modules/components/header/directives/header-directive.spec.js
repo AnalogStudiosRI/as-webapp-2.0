5adcae8f764f0ded44d3e7a8fdd591c9032ff490
Owen Buckley <owen@analogstudios.net>
2015-10-25 14:27:29 -0400

    fixed failing tests

--- src/modules/components/header/directives/header-directive.spec.js
+++ src/modules/components/header/directives/header-directive.spec.js
@@ -20,18 +20,8 @@ describe('as.components.header.directives.HeaderDirectiveTest', function () {
     expect(element.find('header').length).toBe(1);
   });
 
-  it('should test that the navigation menu exist', function () {
-    expect(element.find('as-navigation').length).toBe(1);
-  });
-
-  it('should test that social badges are there', function () {
-    expect(element.find('i').length).toBe(3);
-  });
-
-  it('should test that it says Analog Studios', function () {
-    var studioName = angular.element(element[0].querySelector('div.studio-name')).html();
-
-    expect(studioName).toBe('Analog Studios');
+  it('should test that two banner images exist', function () {
+    expect(element.find('img').length).toBe(2);
   });
 
 });
