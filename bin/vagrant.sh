8db516a5d2d66f882d1083e26d1b35d2dbb4789b
Owen Buckley <obuckley@kenzan.com>
2015-11-07 22:01:08 -0800

    updated vagrant to use latest node

--- bin/vagrant.sh
+++ bin/vagrant.sh
@@ -10,24 +10,37 @@ apt-get -y install vim curl git libfontconfig > /dev/null 2>&1
 apt-get update
 
 echo -e "*** Installing Node and NPM ***"
-apt-get -y -qq install build-essential nodejs-legacy npm
+# remove existing nodejs install
+#apt-get remove --purge node
+#apt-get remove --purge nodejs
 
-sudo npm install -g gulp@3.8.11 bower@1.3.12 npm@2.1.12
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get install -y nodejs
+apt-get install --yes build-essential
+#apt-get install -y npm
 
-echo "bower -v"
-bower -v
+#apt-get -y -qq install build-essential nodejs-legacy npm
+#npm install -g gulp@3.8.11 gulp-cli@0.3.0 bower@1.6.5
+
 echo "node -v"
 node -v
-echo "npm -v"
-npm -v
-echo "gulp -v"
-gulp -v
+#echo "npm -v"
+#npm -v
+#echo "bower -v"
+#bower -v
+#echo "gulp -v"
+#gulp -v
 
-echo -e "*** Installing Project Dependencies (if needed) ***"
-if [ ! -d /vagrant/node_modules ]; then
-  npm install
-fi
+#echo -e "*** Installing Project Dependencies ***"
+#cd /vagrant
 
-if [ ! -d /vagrant/bower_components ]; then
-  bower install
-fi
+#rm -rf node_modules > /dev/null 2>&1
+#rm -rf bower_components > /dev/null 2>&1
+
+#if [ ! -d /vagrant/node_modules ]; then
+#npm install
+#fi
+
+#if [ ! -d /vagrant/bower_components ]; then
+#bower install
+#fi
