0ae6435a26a8a46f1807356789c5ba5109defd76
Owen Buckley <obuckley@kenzan.com>
2015-11-07 22:35:40 -0800

    got node, npm, gulp, and bower installed

--- bin/vagrant.sh
+++ bin/vagrant.sh
@@ -10,37 +10,31 @@ apt-get -y install vim curl git libfontconfig > /dev/null 2>&1
 apt-get update
 
 echo -e "*** Installing Node and NPM ***"
-# remove existing nodejs install
-#apt-get remove --purge node
-#apt-get remove --purge nodejs
-
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 apt-get install -y nodejs
 apt-get install --yes build-essential
-#apt-get install -y npm
 
-#apt-get -y -qq install build-essential nodejs-legacy npm
-#npm install -g gulp@3.8.11 gulp-cli@0.3.0 bower@1.6.5
+npm install -g gulp@3.9.0 bower@1.6.5
 
 echo "node -v"
 node -v
-#echo "npm -v"
-#npm -v
-#echo "bower -v"
-#bower -v
-#echo "gulp -v"
-#gulp -v
+echo "npm -v"
+npm -v
+echo "bower -v"
+bower -v
+echo "gulp -v"
+gulp -v
 
-#echo -e "*** Installing Project Dependencies ***"
-#cd /vagrant
+echo -e "*** Installing Project Dependencies ***"
+cd /vagrant
 
-#rm -rf node_modules > /dev/null 2>&1
-#rm -rf bower_components > /dev/null 2>&1
+rm -rf node_modules > /dev/null 2>&1
+rm -rf bower_components > /dev/null 2>&1
 
 #if [ ! -d /vagrant/node_modules ]; then
-#npm install
+npm install
 #fi
 
 #if [ ! -d /vagrant/bower_components ]; then
-#bower install
+bower install -y
 #fi
