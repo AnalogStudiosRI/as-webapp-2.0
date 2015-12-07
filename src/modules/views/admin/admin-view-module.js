6dc51ef427434aced4bdc0aba3c9527c5121ad2b
Owen Buckley <owen@analogstudios.net>
2015-12-06 19:01:27 -0500

    AS-175 create basic event HTML / CSS

--- src/modules/views/admin/admin-view-module.js
+++ src/modules/views/admin/admin-view-module.js
@@ -4,7 +4,8 @@
 
   angular
     .module('as.views.admin', [
-      'as.components.authentication'
+      'as.components.authentication',
+      'as.components.events'
     ]);
 
 }(angular));
--- src/modules/views/admin/controllers/admin-view-events-controller.js
+++ src/modules/views/admin/controllers/admin-view-events-controller.js
@@ -6,12 +6,24 @@
     .module('as.views.admin')
     .controller('AdminViewEventsController', adminViewEventsController);
 
-  adminViewEventsController.$inject = ['$log'];
+  adminViewEventsController.$inject = ['$log', 'EventsFactory'];
 
   function adminViewEventsController($log) {
     $log.info('ENTER as.views.admin.events');
     /*jshint validthis:true */
-    //var vm = this;
+    var vm = this;
+
+    vm.event = {
+      title: '',
+      description: '',
+      date: ''
+    };
+
+    vm.submitEvent = function () {
+      $log.debug(vm.event);
+      $log.debug();
+    };
+
   }
 
 }(angular));
