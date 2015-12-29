b93a763e6a3b2cc63cc2b088b0bd307db6a17b11
Owen Buckley <owen@analogstudios.net>
2015-12-29 15:18:08 -0500

    adding events to the home page

--- src/modules/views/home/controllers/home-page-controller.js
+++ src/modules/views/home/controllers/home-page-controller.js
@@ -6,12 +6,30 @@
     .module('as.views.home')
     .controller('HomeViewController', homeViewContoller);
 
-  homeViewContoller.$inject = ['$log', 'LanguageFactory'];
+  homeViewContoller.$inject = ['$log', 'LanguageFactory', 'EventsFactory'];
 
-  function homeViewContoller($log, LanguageFactory) {
+  function homeViewContoller($log, LanguageFactory, EventsFactory) {
     /*jshint validthis:true */
     var vm = this;
     var LANG = LanguageFactory.get();
+
+    $scope.events = [];
+
+    function parseEventsResponse(response) {
+      var events = [];
+
+      _.forEach(response, function (n) {
+        var event = n;
+        var time = parseInt(event.startTime * 1000);
+        var eventDate = new Date(time);
+
+        event.date = eventDate;
+
+        events.push(event);
+      });
+
+      return events;
+    }
 
     //public members
     vm.welcomeText = LANG.PAGE.HOME.WELCOME;
@@ -19,10 +37,21 @@
     //public methods
     vm.init = function () {
       $log.info('Enter AS.HomeView.init');
+      EventsFactory.query(function (data) {
+        $log.debug('!!!!!!');
+        $log.debug(data);
+        $scope.events = parseEventsResponse(data);
+      }, function() {
+        $log.error('omg');
+      });
     };
 
     //init
     vm.init();
+
+    vm.eventClick = function (data) {
+      $log.debug(data);
+    };
   }
 
 }(angular));
