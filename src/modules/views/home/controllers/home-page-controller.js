5f2d1fa563eec132a33939d834cb737dc5b5ce36
Owen Buckley <owen@analogstudios.net>
2015-12-30 11:53:43 -0500

    fix issue with controllers being called twice

--- src/modules/views/home/controllers/home-page-controller.js
+++ src/modules/views/home/controllers/home-page-controller.js
@@ -6,25 +6,23 @@
     .module('as.views.home')
     .controller('HomeViewController', homeViewContoller);
 
-  homeViewContoller.$inject = ['$scope', '$log', 'LanguageFactory'];
+  homeViewContoller.$inject = ['$log', 'LanguageFactory'];
 
-  function homeViewContoller($scope, $log, LanguageFactory) {
-    //controller
-    var HomeCtrl = {};
+  function homeViewContoller($log, LanguageFactory) {
+    /*jshint validthis:true */
+    var vm = this;
     var LANG = LanguageFactory.get();
 
     //public members
-    HomeCtrl.welcomeText = LANG.PAGE.HOME.WELCOME;
+    vm.welcomeText = LANG.PAGE.HOME.WELCOME;
 
     //public methods
-    HomeCtrl.init = function () {
+    vm.init = function () {
       $log.info('Enter AS.HomeView.init');
     };
 
     //init
-    HomeCtrl.init();
-
-    $scope.HomeCtrl = HomeCtrl;
+    vm.init();
   }
 
 }(angular));
