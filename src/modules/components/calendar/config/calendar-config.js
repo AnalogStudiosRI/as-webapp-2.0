663d679fc645526c4e22df6e74c481bb9eeb6ee3
Owen Buckley <owen@analogstudios.net>
2015-12-31 17:46:19 -0500

    AS-214 fixed stying of calendar days when not viewing the current month

--- src/modules/components/calendar/config/calendar-config.js
+++ src/modules/components/calendar/config/calendar-config.js
@@ -19,12 +19,15 @@
       $delegate[0].templateUrl = '/components/calendar/templates/datapicker.html';
 
       //https://gist.github.com/cgmartin/3daa01f910601ced9cd3
+      //http://stackoverflow.com/questions/32656556/detect-the-selected-month-in-angular-ui-bootstrap-datepicker
       directive.compile = function() {
+
         return function(scope, element, attrs, ctrls) {
           link.apply(this, arguments);
 
           var datepickerCtrl = ctrls[0];
           var ngModelCtrl = ctrls[1];
+          var originalMove = scope.move;
 
           if (ngModelCtrl) {
             // Listen for 'refreshDatepickers' event...
@@ -32,6 +35,13 @@
               datepickerCtrl.refreshView();
             });
           }
+
+          scope.move = function() {
+            originalMove.apply(this, arguments);
+            //when move is called, the 'this' object contains a 'title' property with the current month!
+            //emit the event to parent scopes
+            scope.$emit('datepicker.monthChanged', this.title);
+          };
         };
       };
 
