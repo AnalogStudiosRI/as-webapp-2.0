41478f9cb3403acc0ca91bfa8ec582da07501488
Owen Buckley <owen@analogstudios.net>
2015-12-31 19:15:41 -0500

    AS-215 fixed bug where url did not change on a new page view

--- src/modules/components/social-share/directives/social-share-directive.js
+++ src/modules/components/social-share/directives/social-share-directive.js
@@ -5,15 +5,19 @@
     .module('as.components.socialShare')
     .directive('asSocialShare', asSocialShareDirective);
 
-  asSocialShareDirective.$inject = ['$log', '$location'];
+  asSocialShareDirective.$inject = ['$log', '$rootScope', '$location'];
 
-  function asSocialShareDirective($log, $location) {
+  function asSocialShareDirective($log, $rootScope, $location) {
 
     return {
       restrict: 'E',
       templateUrl: '/components/social-share/social-share-directive-template.html',
       link: function (scope) {
         scope.url = $location.absUrl();
+
+        $rootScope.$on('$locationChangeSuccess', function() {
+          scope.url = $location.absUrl();
+        });
       }
     };
   }
