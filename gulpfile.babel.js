00abcad08bad1d3ec053a750ab3c60d5bd0c3a6c
Owen Buckley <owen@analogstudios.net>
2016-06-16 23:56:19 -0400

    clean up pipelines

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -147,4 +147,199 @@ gulp.task('serve', function () {
 //     "run-sequence": "1.1.0",
 //     "wiredep": "2.2.2",
 //     "yargs": "3.10.0"
+// 
+// var AWS = require('aws-sdk');
+// var fs = require('fs');
+// var glob = require('glob');
+// var gulp = require('gulp');
+//
+// AWS.config.region = 'us-west-2';
+//
+// function getContentType(extension) {
+//   var contentType = '';
+//
+//   switch (extension) {
+//     case '.eot':
+//       contentType = 'application/vnd.ms-fontobject';
+//       break;
+//     case '.jpg':
+//       contentType = 'image/jpeg';
+//       break;
+//     case '.js':
+//       contentType = 'application/javascript';
+//       break;
+//     case '.otf':
+//       contentType = 'application/x-font-opentype';
+//       break;
+//     case '.png':
+//       contentType = 'image/png';
+//       break;
+//     case '.svg':
+//       contentType = 'image/svg+xml';
+//       break;
+//     case '.ttf':
+//       contentType = 'application/x-font-ttf';
+//       break;
+//     case '.woff':
+//       contentType = 'application/font-woff';
+//       break;
+//     case '.woff2':
+//       contentType = 'application/font-woff2';
+//       break;
+//     default:
+//       contentType = 'text/' + extension.replace('.', '');
+//       break;
+//   }
+//
+//   return contentType;
 // }
+//
+// function httpUploadProgress(evt) {
+//   console.log(evt);
+// }
+//
+// function httpUploadSend(err, data) {
+//   console.log(err, data);
+// }
+//
+//
+// gulp.task('s3:deploy', function() {
+//   s3.listBuckets(function(err, data) {
+//    if (err) {
+//      console.log("Error:", err);
+//    } else {
+//      for (var index in data.Buckets) {
+//        var bucket = data.Buckets[index];
+//        console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+//      }
+//    }
+//   });
+//
+//   // options is optional
+//   glob('dest/**/*', {}, function (er, files) {
+//     for (var i = 0, l = files.length; i < l; i += 1) {
+//       var filename = files[i];
+//       var s3filename = filename.replace('dest/', '');
+//
+//       //upload only files
+//       //XXX hack to get around odd spin.js build oddity
+//       //XXX would not be an issue if AS-219, AS-225
+//       if (filename.indexOf('.') > 0 && filename !== 'dest/assets/js/vendor/spin.js') {
+//         var extension = filename.slice(filename.lastIndexOf('.'));
+//         var contentType = getContentType(extension);
+//         var body = fs.createReadStream(filename); //.pipe(zlib.createGzip());
+//
+//         var s3 = new AWS.S3({
+//           params: {
+//             Bucket: 'static.analogstudios.net',
+//             Key: s3filename,
+//             ContentType: contentType
+//           }
+//         });
+//
+//         s3.upload({Body: body}).on('httpUploadProgress', httpUploadProgress).send(httpUploadSend);
+//       }
+//     }
+//   });
+//
+// }
+//
+// );
+//application related javascript gulp tasks
+//directory context is the cwd of gulpfile.js (/webapp/)
+// 'use strict';
+//
+// var loadPluginsConfig = require('./configs/load-plugin-config.json');
+// var $ = require('gulp-load-plugins')(loadPluginsConfig);
+// var gulp = require('gulp-param')(require('gulp'), process.argv);
+// var packageJson = require('../package.json');
+//
+// gulp.task('js:lint', function () {
+//
+//   return gulp.src(['*.js', './pipelines/*', './src/**/**/*.js'])
+//     .pipe($.jshint())
+//     .pipe($.jscs())
+//     .pipe($.jscsStylish.combineWithHintResults())
+//     .pipe($.jshint.reporter('jshint-stylish'))
+//     .pipe($.jshint.reporter('fail'));
+//
+// });
+//
+// //lints and builds local or production JS based on --production flag
+// gulp.task('js:build', function (production) {
+//   var isProductionBuild = production ? true : false;
+//   var filename = packageJson.name + '-' + packageJson.version + '.min.js';
+//
+//   return gulp.src(['!**/**/*.spec.js', '**/**/*-module.js', '**/**/*.js'], {base: './src', cwd: './src'})
+//     .pipe($.if(isProductionBuild, $.concat(filename)))
+//     .pipe($.if(isProductionBuild, $.uglify()))
+//     .pipe(gulp.dest('./dest/assets/js/'));
+//
+// });
+//
+//CSS related gulp tasks
+//directory context is the cwd of gulpfile.js (/webapp/)
+// 'use strict';
+//
+// var loadPluginsConfig = require('./configs/load-plugin-config.json');
+// var $ = require('gulp-load-plugins')(loadPluginsConfig);
+// var gulp = require('gulp-param')(require('gulp'), process.argv);
+// var packageJson = require('../package.json');
+//
+// var lessLintTasks = $.lazypipe()
+//   .pipe($.less)
+//   .pipe($.csslint, 'csslintrc.json')
+//   .pipe($.csslint.reporter)
+//   .pipe($.csslint.failReporter);
+//
+// //convenience task for just linting CSS
+// gulp.task('less:lint', function () {
+//
+//   return gulp.src(['./src/**/**/*.less', '!./src/less/as-webapp.less'])
+//     .pipe(lessLintTasks());
+//
+// });
+//
+// //compiles application less, lints the CSS, and generates developer or production CSS based on --production flag
+// gulp.task('less:build', function (production) {
+//   var isProductionBuild = production ? true : false;
+//   var lessFiles = ['./src/less/as-webapp.less', './src/**/**/*.less'];
+//   var filename = packageJson.name + '-' + packageJson.version + '.min.css';
+//
+//   return gulp.src(lessFiles)
+//     .pipe($.less())
+//     .pipe($.if(isProductionBuild, $.concat(filename)))
+//     .pipe($.autoprefixer({
+//       browsers: ['last 2 versions', 'Explorer >= 9']
+//     }))
+//     .pipe($.if(isProductionBuild, $.minifyCss()))
+//     .pipe(gulp.dest('./dest/assets/css'));
+//
+// });
+//
+//
+//
+// var gulp = require('gulp');
+// var karma = require('karma').server;
+//
+// //run tests and keep runner open, for doing test driven development
+// gulp.task('test:tdd', function () {
+//
+//   karma.start({
+//     configFile: __dirname + '/../karma.conf.js',
+//     autoWatch: true,
+//     singleRun: false,
+//     browsers: ['Chrome']
+//   });
+//
+// });
+//
+// //will run all tests and generate coverage output, primarily for Jenkins
+// gulp.task('test:coverage', function () {
+//
+//   return karma.start({
+//     configFile: __dirname + '/../karma-coverage.conf.js',
+//     browsers: ['PhantomJS']
+//   });
+//
+// });
