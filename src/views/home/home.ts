67df46d55bce2b3b9285e766d8028a9aa2d07938
Owen Buckley <owen@analogstudios.net>
2016-06-26 21:27:19 -0400

    templateUrl for the home page

--- src/views/home/home.ts
+++ src/views/home/home.ts
@@ -1,10 +1,65 @@
-console.log('ENTER HOME');
+'use strict';
 
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: '<h1>The Home Page!!!</h1>'
+  templateUrl: './src/views/home/home.html'
 })
 
 export class HomeView { }
+
+//
+// 'use strict';
+//
+// (function (angular) {
+//
+//   angular
+//     .module('as.views.home')
+//     .controller('HomeViewController', homeViewContoller);
+//
+//   homeViewContoller.$inject = ['$log', '$state', 'EventsFactory'];
+//
+//   function homeViewContoller($log, $state, EventsFactory) {
+//     /*jshint validthis:true */
+//     var vm = this;
+//
+//     vm.events = [];
+//
+//     function parseEventsResponse(response) {
+//       var events = [];
+//
+//       _.forEach(response, function (n) {
+//         var event = n;
+//         var time = parseInt(event.startTime * 1000);
+//         var eventDate = new Date(time);
+//
+//         event.date = eventDate;
+//
+//         events.push(event);
+//       });
+//
+//       return events;
+//     }
+//
+//     //public methods
+//     vm.init = function () {
+//       $log.info('Enter AS.HomeView.init');
+//
+//       EventsFactory.query(function (data) {
+//         vm.events = parseEventsResponse(data);
+//       }, function(response) {
+//         $log.error(response);
+//       });
+//     };
+//
+//     //init
+//     vm.init();
+//
+//     vm.eventClick = function (eventData) {
+//       $log.debug(eventData);
+//       $state.go('events-view-detailed', eventData);
+//     };
+//   }
+//
+// }(angular));
