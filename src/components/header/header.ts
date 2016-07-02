3da0523f50aedab1c083765cc66b5494ef4366ba
Owen Buckley <owen@analogstudios.net>
2016-07-02 17:48:32 -0400

    header restored

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'as-header',
-  templateUrl: './src/components/header/header.html'
+  templateUrl: './src/components/header/header.html',
+  styleUrls: ['./src/components/header/header.css']
 })
 
 export class HeaderComponent { }
