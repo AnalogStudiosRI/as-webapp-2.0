02de877031d2694884ec7eda01e251143e5810ea
Owen Buckley <owen@analogstudios.net>
2016-07-02 18:56:53 -0400

    basic footer, no social share

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'as-footer',
-  templateUrl: './src/components/footer/footer.html'
-  //styleUrls: ['./src/components/footer/footer.css']
+  templateUrl: './src/components/footer/footer.html',
+  styleUrls: ['./src/components/footer/footer.css']
 })
 
-export class FooterComponent { }
+export class FooterComponent {
+  private currentYear: string;
+
+  constructor(){
+    this.currentYear = new Date().getFullYear().toString();
+  }
+
+  getCurrentYear() {
+    return this.currentYear;
+  }
+}
