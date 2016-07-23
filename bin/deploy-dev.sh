ca26dfed9e99526cbdabe8db83b6d4c69a417dc1
Owen Buckley <owen@analogstudios.net>
2016-07-23 02:06:42 -0400

    update deploy script

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -10,6 +10,6 @@ done
 
 echo "deploy to webroot..."
 sudo cp -vf index.html /var/www/analogstudios/html/
-sudo cp -rvf src/* /var/www/analogstudios/html/
+sudo cp -rvf src/ /var/www/analogstudios/html/
 sudo cp -rvf node_modules/ /var/www/analogstudios/html/
 sudo cp -vf systemJS.config.js /var/www/analogstudios/html/systemjs.config.js
