ce9f595e90ce516181a5db4e5799b8b06562dfca
Owen Buckley <owen@analogstudios.net>
2016-08-14 16:22:02 -0400

    remove path ref

--- src/views/events/events.component.ts
+++ src/views/events/events.component.ts
@@ -4,7 +4,7 @@ import { EventsCalendarComponent } from '../../components/events-calendar/events
 @Component({
   selector: 'events',
   templateUrl: './src/views/events/events.html',
-  styleUrls: ['./node_modules/bootstrap/dist/css/bootstrap.min.css', './src/views/events/events.css'],
+  styleUrls: ['./node_modules/bootstrap/dist/css/bootstrap.min.css'],
   directives: <any>[EventsCalendarComponent]
 })
 
