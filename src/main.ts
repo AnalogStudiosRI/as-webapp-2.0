d92b4fe8b3a1605212023f87e8004d89240e5d72
Owen Buckley <owen@analogstudios.net>
2016-08-28 08:56:06 -0400

    got local storage working

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,63 @@
-// TODO LocalStorageSubscriber(appPromise);
-
+//TODO make an admin module - https://thegreenhouse.atlassian.net/browse/AS-285
+import { AdminViewComponent } from './views/admin/admin.component';
+import { AdminViewEventsComponent } from './views/admin/admin-events.component';
+import { AdminViewPostsComponent } from './views/admin/admin-posts.component';
+import { APP_ROUTES } from './routes';
+import { AuthenticationService } from './services/authentication.service';
+import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { EventsCalendarComponent } from './components/events-calendar/events-calendar.component';
+import { EventDetailsViewComponent } from './views/events/event-details.component';
+import { EventsService } from './services/events.service';
+import { EventsViewComponent } from './views/events/events.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeViewComponent } from './views/home/home.component';
+import { HttpModule } from '@angular/http';
+import { JwtHelper } from 'angular2-jwt/angular2-jwt';
+import { LocalStorage, WEB_STORAGE_PROVIDERS } from 'h5webstorage';
+import { NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { PostsService } from './services/posts.service';
+import { RouterModule } from '@angular/router';
+
+
+//TODO EventsCalendarComponent and PostsListComponent?
+//Is this still good for module bundling by putting everything here
+//or does angular / webpack still optimize?
+@NgModule({
+  imports: [ //modules
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(APP_ROUTES)
+  ],
+  declarations: [ //component and directives, order matters!! )for now)
+    EventsCalendarComponent,
+    PostsListComponent,
+
+    FooterComponent,
+    HeaderComponent,
+
+    EventDetailsViewComponent,
+    EventsViewComponent,
+    HomeViewComponent,
+    AdminViewEventsComponent,
+    AdminViewPostsComponent,
+    AdminViewComponent,
+    BootstrapComponent
+  ],
+  bootstrap: [ BootstrapComponent ],  //root component
+  providers: [ //services (eg. @injectables)
+    LocalStorage,
+    WEB_STORAGE_PROVIDERS,
+    AuthenticationService,
+    EventsService,
+    JwtHelper,
+    PostsService
+  ]
+})
+
+class AppModule { }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,28 +1,33 @@
 import { Http, Response } from "@angular/http";
 import { Injectable } from '@angular/core';
 import { JwtHelper } from 'angular2-jwt/angular2-jwt';
-import { LocalStorage } from 'angular2-localstorage/WebStorage';
+import { LocalStorage } from 'h5webstorage';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class AuthenticationService {
   //TODO constant - https://thegreenhouse.atlassian.net/browse/AS-246
   private API_URL_LOGIN: string = '/api/login';
-  @LocalStorage('token') private token: string = null;
 
   //TODO $httpInterceptor? - https://thegreenhouse.atlassian.net/browse/AS-256
-  constructor(private Http: Http, private JwtHelper: JwtHelper){
-    if (this.token && !this.isValidToken(this.token)) {
+  constructor(private Http: Http, private JwtHelper: JwtHelper, private LocalStorage: LocalStorage){
+    let token: string = this.LocalStorage['token'] || null;
+
+    if (token && !this.isValidToken(token)) {
       this.clearToken();
     }
   }
 
+  private getTokenFromLocalStorage() {
+    return this.LocalStorage['token'];
+  }
+
   private setToken(value: string) {
-    this.token = this.isValidToken(value) ? value : null;
+    this.LocalStorage.setItem('token', this.isValidToken(value) ? value : null);
   }
 
   private clearToken(): void {
-    this.token = null;
+    this.LocalStorage.setItem('token', null);
   }
 
   private isValidToken(tokenToValidate: string): boolean {
@@ -63,11 +68,11 @@ export class AuthenticationService {
   }
 
   public isAuthenticated(): boolean {
-    return this.isValidToken(this.token);
+    return this.isValidToken(this.getTokenFromLocalStorage());
   }
 
   public getToken(): string {
-    return this.token;
+    return this.getTokenFromLocalStorage();
   }
 
 }
