4b9c9d79bac074e75bd3acb29388ab1e31f4ebc1
Owen Buckley <owen@analogstudios.net>
2016-09-08 20:00:00 -0400

    create basic artists page

--- src/views/artists/artists.component.ts
+++ src/views/artists/artists.component.ts
@@ -1,27 +1,39 @@
+import { ArtistInterface, ArtistsService } from '../../services/artists.service';
 import { Component, OnInit } from '@angular/core';
-import {ArtistsService, ArtistInterface} from '../../services/artists.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'artists',
+  selector: 'artists-view',
   templateUrl: './artists.html',
   styleUrls: [ './artists.less' ]
 })
 
-export class ArtistsViewComponent extends OnInit {
+export class ArtistsViewComponent implements OnInit {
   private artists: Array<ArtistInterface> = [];
 
-  constructor(private ArtistsService: ArtistsService) {
-    super();
+  constructor(private Router: Router, private ArtistsService: ArtistsService){
+
   }
 
   ngOnInit(): void {
     this.ArtistsService.getArtists().subscribe((data: Array<ArtistInterface>) => {
-      console.log('artists data', data);
       this.artists = data;
     })
   }
 
-  public getArtists(): Array<ArtistInterface> {
+  private gotoAlbumDetailsView(albumId: number) {
+    this.Router.navigate(['artists', albumId]);
+  }
+
+  public getAlbums(): Array<ArtistInterface> {
     return this.artists;
+  }
+
+  public onAlbumSelected(selectedIndex: number){
+    this.gotoAlbumDetailsView(this.artists[selectedIndex].id);
+  }
+
+  public onAlbumClicked(artist: ArtistInterface){
+    this.gotoAlbumDetailsView(artist.id);
   }
 }
