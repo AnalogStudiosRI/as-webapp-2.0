c0609966527dc4b36faf12ee34d0ce46a52d752b
Owen Buckley <owen@analogstudios.net>
2016-09-30 00:06:29 -0400

    contact page files

---
+++ src/views/contact/contact.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'as-view-contact',
+  templateUrl: './contact.html',
+  styleUrls: [ './contact.scss' ]
+})
+
+export class ContactViewComponent { }
