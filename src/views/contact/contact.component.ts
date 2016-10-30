77a074c67ee179c3230d39f1949fec95c5b33985
Owen Buckley <owen@analogstudios.net>
2016-10-30 08:37:41 -0400

    basic contact form markup and styles

--- src/views/contact/contact.component.ts
+++ src/views/contact/contact.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { FormBuilder, FormGroup, FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES} from "@angular/forms";
 
 @Component({
-  selector: 'as-view-contact',
+  selector: 'contact-view',
   templateUrl: './contact.html',
-  styleUrls: [ './contact.scss' ]
+  styleUrls: [ './contact.scss' ],
+  directives: [ CORE_DIRECTIVES, FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES ],
+  providers: [ FormBuilder ],
 })
 
-export class ContactViewComponent { }
+export class ContactViewComponent {
+  public contactForm: FormGroup;
+
+  constructor(private FormBuilder: FormBuilder) {
+    this.contactForm = this.FormBuilder.group({
+      subject: '',
+      message: ''
+    })
+  }
+
+  public contact(): void {
+    let subject: string = this.contactForm.controls['subject'].value;
+    let message: string = this.contactForm.controls['message'].value;
+
+    console.log('subject', subject);
+    console.log('message', message);
+  }
+}
