1bacaba5298dada25bc54be8e21634f8c8d63ed9
Owen Buckley <obuckley@kenzan.com>
2016-11-03 21:36:13 -0400

    clean up pipe implementation

--- src/pipes/ellipsis.pipe.ts
+++ src/pipes/ellipsis.pipe.ts
@@ -1,19 +1,14 @@
 import { Pipe } from '@angular/core';
 
 @Pipe({
-    name: 'ellipsis'
+  name: 'ellipsis'
 })
-
 export class EllipsisPipe {
-  transform(val, args) {
-    if (args === undefined) {
-      return val;
+  transform(value: string, limit: number) {
+    if (!limit) {
+      return value;
     }
 
-    if (val.length > args) {
-      return val.substring(0, args) + '...';
-    } else {
-      return val;
-    }
+    return value.length > limit ? value.substring(0, limit) + '...' : value;
   }
 }
